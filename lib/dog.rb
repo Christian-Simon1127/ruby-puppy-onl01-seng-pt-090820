3eac81fa6d280a1b573e832e9b7eaad26785eeed
Christian Simon <csimon1127@gmail.com>
2020-09-28 18:33:00 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
 # Add your code here
+
+class Dog 
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    #@@all << name
+  end 
+  
+  def save 
+    @@all << self.name
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def self.print_all
+    @all.each { |item|
+      puts item
+    }
+  end 
+  
+end
